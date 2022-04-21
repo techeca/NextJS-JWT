7dcdb4924ef1b39bde49b62187c17d578bd7d76d
techeca <jp.vasquez@outlook.com>
2022-04-21 06:34:11 -0400

    test4

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = {
   publicRuntimeConfig: {
     apiUrl: process.env.NODE_ENV === 'development'
      ? 'http://localhost:3000/api' // dev
-     : 'http://localhost:3000/api' // production
+     : 'localhost:3000/api' // production
   },
   images: {
     loader:'imgix',

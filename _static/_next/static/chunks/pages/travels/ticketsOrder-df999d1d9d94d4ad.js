6c580a1d6c87a0c03e1171447a569be92a468d3c
techeca <53408118+techeca@users.noreply.github.com>
2022-04-26 10:14:07 -0400

    Delete _static directory

--- _static/_next/static/chunks/pages/travels/ticketsOrder-df999d1d9d94d4ad.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[428],{1037:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/travels/ticketsOrder",function(){return u(4082)}])}},function(n){n.O(0,[655,898,385,582,365,824,247,332,202,82,774,888,179],(function(){return _=1037,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
